2f77943f0ff1899608d44c742bd0d30240b95357
moreC <longbaobao_today@163.com>
2016-10-02 23:17:21 +0800

    use template

--- src/code.py
+++ src/code.py
@@ -4,6 +4,7 @@
 import web #import the package for web
 
 urls =  ("/", "index", "/test*", "test")
+render = web.template.render('templates')
 def readhtml(htmlfile):
     # read html file to string
     return open(htmlfile, 'rb').read()
@@ -18,8 +19,9 @@ class test():
         user_data = web.input()
         fisrt_name = user_data.FirstName
         last_name = user_data.LastName
-        return "<h1>Your input name:</h1><h3><font color='blue'> %s %s</font></h3>"\
-                % (fisrt_name, last_name)
+        return render.greeting(fisrt_name, last_name)
+        #return "<h1>Your input name:</h1><h3><font color='blue'> %s %s</font></h3>"\
+        #        % (fisrt_name, last_name)
 
 if __name__ == '__main__':
     app = web.application(urls, globals())
